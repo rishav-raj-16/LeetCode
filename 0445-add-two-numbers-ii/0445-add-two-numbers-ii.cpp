f3f3edc6154e7f3a1f544556d33dcb7f5f2779b8
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-02-10 22:03:01 +0530

    Time: 50 ms (33.37%), Space: 71.4 MB (46.05%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // Function to reverse the List....(●'◡'●)
+    ListNode* reverse(ListNode*head)
+    {
+        // Initialize current, previous and next pointers
+        ListNode* current = head;
+        ListNode *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+            // Reverse current node's pointer
+            current->next = prev;
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //revese both list by revers function defined above
+        l1=reverse(l1);
+        l2=reverse(l2);
+        ListNode* dummy=new ListNode();
+        ListNode* temp=dummy;
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum=0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            ListNode* newnode=new ListNode(sum%10);
+            temp->next=newnode;
+            temp=temp->next;
+        }
+        dummy=dummy->next;
+        // reverse the resultant list as well
+        return reverse(dummy);
+        // if it helps plzz dont't forget to upvote it \U0001f609
+    }
+};
