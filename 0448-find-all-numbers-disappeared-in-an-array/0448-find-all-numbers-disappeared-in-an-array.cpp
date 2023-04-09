c2b0eb144d07cd7aad45b83404fccaf564fb9dc9
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-04-09 21:22:21 +0530

    Time: 108 ms (33.75%), Space: 33.7 MB (86.19%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-    bool find_el(vector<int>&arr, int a){
-        int low = 0, high = arr.size() - 1;
-        while(low <= high){
-            int mid = (high + low) / 2;
-            if(arr[mid] == a){
-                return false;
-            }
-            else if(a < arr[mid]){
-                high = mid -1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return true;
-    }
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int> res; 
         for(int i = 1; i <= nums.size(); i++){
-            if(find_el(nums,i))
+            if(!binary_search(nums.begin(),nums.end(),i))
                 res.push_back(i);
         }
         return res;
