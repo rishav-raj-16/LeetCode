ba29a71a57ab2a215c83dd71dfb9ca71dbd5b211
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-05-03 23:37:55 +0530

    Time: 64 ms (55.08%), Space: 31.7 MB (67.27%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        vector<vector<int>> answer;
-        set<int> v1;
-        set<int> v2;
+        unordered_set<int> s1(nums1.begin(),nums1.end());
+        unordered_set<int> s2(nums2.begin(),nums2.end());
         
-        for(auto it: nums1){
-            if(check(it,nums2)){
-                v1.insert(it);
-            }
+        vector<int> v1, v2;
+        for(auto x: s1){
+            if(s2.find(x) == s2.end())
+                v1.push_back(x);
         }
-        vector<int> a(v1.begin(),v1.end());
-        answer.push_back(a);
         
-        for(auto it: nums2){
-            if(check(it,nums1)){
-                v2.insert(it);
-            }
+        for(auto x: s2){
+            if(s1.find(x) == s1.end())
+                v2.push_back(x);
         }
-        vector<int> b(v2.begin(),v2.end());
-        answer.push_back(b);
-        return answer;
-    }
-    bool check(int x, vector<int> nums){
-        int low = 0, high = nums.size()-1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == x){
-                return false;
-            }
-            else if(nums[mid] < x){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-        }
-        return true;
+        return {v1, v2};
     }
 };
