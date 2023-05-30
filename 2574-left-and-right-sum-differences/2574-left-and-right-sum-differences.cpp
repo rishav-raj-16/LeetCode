a7ce719124f8590fd71bf428fd16e5d8a8d0deb5
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-05-30 18:04:38 +0530

    Time: 4 ms (90.79%), Space: 11.5 MB (23.18%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -9,14 +9,10 @@ public:
         // submission of left
         for(int i = 1; i < n; i++){
             left[i] = left[i-1]+nums[i-1];
-            cout<<left[i]<<" ";
         }
-        
-        cout<<endl;
         // submission of right
         for(int i = n-2; i >= 0; i--){
             right[i] = right[i+1]+nums[i+1];
-            cout<<right[i]<<" ";
         }
         
         vector<int> ans;
