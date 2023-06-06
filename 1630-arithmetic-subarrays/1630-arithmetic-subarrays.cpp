5d93ffec55e42a5f1f8944c666380ee2d33d8f75
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-06-07 00:15:24 +0530

    Time: 52 ms (94.17%), Space: 25.8 MB (79.72%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    bool test(vector<int>& nums, int l, int r){
-        vector<int> temp;
-        for(int i = l; i <= r; i++)
-            temp.push_back(nums[i]);
-        sort(temp.begin(),temp.end());
-        int c= temp[1] - temp[0];
-        for(int i = 1; i < temp.size()-1; i++){
-            if(temp[i+1] - temp[i] != c)
-                return false;
-        }
-        return true;
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+    vector<bool> res;
+    for (auto i = 0, j = 0; i < l.size(); ++i) {
+        vector<int> n(begin(nums) + l[i], begin(nums) + r[i] + 1);
+        sort(begin(n), end(n));
+        for (j = 2; j < n.size(); ++j)
+            if (n[j] - n[j - 1] != n[1] - n[0])
+                break;
+        res.push_back(j == n.size());
     }
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r){
-        vector<bool> ans;
-        for(int i = 0; i < l.size(); i++){
-            ans.push_back(test(nums, l[i], r[i]));
-        }
-        return ans;
-    }
+    return res;
+}
 };
