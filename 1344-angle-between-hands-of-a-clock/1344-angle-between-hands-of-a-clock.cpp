f865779c41f4ccf0a02eea8af95b50806b46b2cb
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-06-30 19:06:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (94.25%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double a;
-        if(hour == 12)
-            a = ((30*minutes)/double(60));
-        else
-            a = (30 * hour) + ((30*minutes)/double(60));
-        return min(abs(a-(6 * minutes)),360-abs(a-(6 * minutes)));
+        double minuteAngle = 6.0 * minutes;
+        hour %= 12; // Reduce hour to range [0, 11]
+        double hourAngle = 0.5 * (60 * hour + minutes); // Each hour mark is 30 degrees, and each minute mark is 0.5 degrees
+
+        double angle = std::abs(hourAngle - minuteAngle);
+        angle = std::min(angle, 360.0 - angle);
+        return angle;
     }
 };
