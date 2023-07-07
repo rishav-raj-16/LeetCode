321ac42227f962ac4da6f553facb7be96a459f2f
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-07-07 16:30:35 +0530

    Time: 92 ms (47.54%), Space: 31 MB (96.16%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int low = 0, high = 0;
-        for(auto it: weights){
+        for (auto it : weights) {
             high += it;
-            low = max(low,it);
+            low = max(low, it);
         }
-        
-        while(low < high){
+
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if(check(weights, days, mid))
+            if (check(weights, days, mid))
                 high = mid;
             else
-                low = mid+1;
+                low = mid + 1;
         }
         return low;
     }
-    bool check(vector<int>& weights, int days, int x){
-        int count = 1, sum = 0;
-        for(int it: weights){
-            sum += it;
-            if(sum > x){
+
+    bool check(vector<int>& weights, int days, int x) {
+        int count = 1;
+        int cumulativeWeight = 0;
+        for (int i = 0; i < weights.size(); i++) {
+            cumulativeWeight += weights[i];
+            if (cumulativeWeight > x) {
                 count++;
-                sum = it;
+                cumulativeWeight = weights[i];
             }
         }
-        return  count <= days;
+        return count <= days;
     }
 };
