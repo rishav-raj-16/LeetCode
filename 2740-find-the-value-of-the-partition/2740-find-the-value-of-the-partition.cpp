e9030e6d3373ab3666357619fa07685030a2506f
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-07-18 22:12:37 +0530

    Time: 154 ms (15.98%), Space: 59.1 MB (46.38%) - LeetHub

--- 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.cpp
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.cpp
@@ -4,11 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         
         int diff = nums[1] - nums[0];
-        for(int i = 0; i < nums.size()-1; i++){
-            int temp = nums[i+1] - nums[i];
-            cout<<temp<<" ";
-            diff = min(diff,temp);
-        }
+        for(int i = 0; i < nums.size()-1; i++)
+            diff = min(diff,nums[i+1] - nums[i]);
+        
         return diff;
     }
 };
