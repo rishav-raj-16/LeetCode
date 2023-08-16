94b0c095230bcdb1d79091a87e695b2254939556
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-08-16 15:50:35 +0530

    Time: 491 ms (8.89%), Space: 75.9 MB (41.11%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> store;
+        long long ans = 0;
+        
+        for(auto it:nums)
+            store.push(it);
+        
+        
+        for(int i = 0; i < k; i++){
+            long long j = store.top();
+            store.pop();
+            
+            cout<<j<<" ";
+            
+            ans += j;
+            j = ceil(j/3.0);
+            
+            store.push(j);
+        }
+        return ans;
+    }
+};
