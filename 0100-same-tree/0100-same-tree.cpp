26edf56cebde51c3e240dfa0d31c2f160477d8a7
rishav-raj-16 <114130800+rishav-raj-16@users.noreply.github.com>
2023-08-30 23:29:30 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (10.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,21 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL || q == NULL) {
-            return (p == q);
+        queue<TreeNode*> q1, q2;
+        
+        q1.push(p);
+        q2.push(q);
+        
+        while(!q1.empty() && !q2.empty()) {
+            TreeNode* c1 = q1.front();
+            TreeNode* c2 = q2.front();
+            q1.pop();
+            q2.pop();
+            
+            if (c1 == nullptr && c2 == nullptr) {
+                continue; // Both nodes are nullptr, so they are equal at this point.
+            }
+            if (c1 == nullptr || c2 == nullptr) {
+                return false; // One node is nullptr while the other is not.
+            }
+            if (c1->val != c2->val) {
+                return false; // Node values are different.
+            }
+            
+            q1.push(c1->left);
+            q1.push(c1->right);
+            q2.push(c2->left);
+            q2.push(c2->right);
         }
         
-        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return q1.empty() && q2.empty(); // Both queues should be empty if trees are identical.
     }
 };
