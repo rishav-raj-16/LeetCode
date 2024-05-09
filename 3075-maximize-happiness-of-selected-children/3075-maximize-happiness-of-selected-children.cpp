f6fed42dffbe9e3b330610299b6da7eb0ec05280
Rishav Raj <114130800+rishav-raj-16@users.noreply.github.com>
2024-05-09 22:34:05 +0530

    Time: 239 ms (5.50%), Space: 108.7 MB (28.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        // ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         long long ans = 0;
         int dec = 0, i = 0;
         
