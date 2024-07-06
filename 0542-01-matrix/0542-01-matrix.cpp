b7f59616b44a89f1d43eb2374e43179cf59c2ae6
Rishav Raj <114130800+rishav-raj-16@users.noreply.github.com>
2024-07-06 12:00:03 +0530

    Time: 51 ms (72.10%), Space: 35.2 MB (72.24%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int vis[n][m];
+        vector<vector<int>> dis(n,vector<int> (m,0));
+        queue<pair<pair<int,int>,int>> q;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(!mat[i][j]){
+                    vis[i][j] = 1;
+                    q.push({{i,j},0});
+                }else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+        
+        int drow[] = {0,0,-1,1};
+        int dcol[] = {-1,1,0,0};
+        
+        while(!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int d = q.front().second;
+            q.pop();
+            dis[row][col] = d;
+            
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                  && !vis[nrow][ncol] && mat[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow,ncol},d+1});
+                }
+            }
+        }
+        return dis;
+    }
+};
