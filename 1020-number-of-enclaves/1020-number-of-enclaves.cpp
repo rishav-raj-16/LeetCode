7e0a6db94d03343c3cf7536071fa67232e1ae780
Rishav Raj <114130800+rishav-raj-16@users.noreply.github.com>
2024-07-06 13:14:49 +0530

    Time: 47 ms (77.89%), Space: 35.5 MB (39.91%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -8,15 +8,15 @@ public:
         
         for(int row = 0; row < n; row++) {
             if(grid[row][0] == 1)
-                bfs(row,0,grid,vis);
+                dfs(row,0,grid,vis);
             if(grid[row][m-1] == 1)
-                bfs(row,m-1,grid,vis);
+                dfs(row,m-1,grid,vis);
         }
         for(int col = 0; col < m; col++) {
             if(grid[0][col] == 1)
-                bfs(0,col,grid,vis);
+                dfs(0,col,grid,vis);
             if(grid[n-1][col] == 1)
-                bfs(n-1,col,grid,vis);
+                dfs(n-1,col,grid,vis);
         }
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
@@ -31,28 +31,18 @@ private:
     int drow[4] = {0,0,-1,1};
     int dcol[4] = {-1,1,0,0};
     
-    void bfs(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& vis){
+    void dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis){
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,int>> q;
-        vis[r][c] = 1;
-        q.push({r,c});
+        vis[row][col] = 1;
         
-        while(!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+        for(int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
             
-            for(int i = 0; i < 4; i++) {
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-                
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-                && grid[nrow][ncol] && !vis[nrow][ncol]){
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
-            }
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+            && grid[nrow][ncol] && !vis[nrow][ncol])
+                dfs(nrow,ncol,grid,vis);
         }
     }
 };
