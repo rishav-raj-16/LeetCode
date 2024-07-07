a94cb8e653ab36dae2aeea8c3640ca60d5eef404
Rishav Raj <114130800+rishav-raj-16@users.noreply.github.com>
2024-07-07 16:31:59 +0530

    Time: 149 ms (29.87%), Space: 65.3 MB (24.09%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,28 +3,21 @@ public:
     // Topological Sort Solution
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         int V = adj.size();
+        queue<int> q;
+        vector<int> ans;
+        vector<int> indegree(V,0);
         vector<vector<int>> nadj(V);
         
         for(int i = 0; i < V; i++) {
-            for(auto it:adj[i])
+            for(auto it:adj[i]){
                 nadj[it].push_back(i);
+                indegree[i]++;
+            }
         }
-        
-        vector<int> indegree(V,0);
-        
-        for(int i = 0; i < V; i++) {
-            for(auto it:nadj[i])
-                indegree[it]++;
-        }
-        
-        queue<int> q;
-        vector<int> ans;
-        
         for(int i = 0 ; i < V; i++) {
             if(!indegree[i])
                 q.push(i);
         }
-        
         while(!q.empty()) {
             int node = q.front();
             ans.push_back(node);
